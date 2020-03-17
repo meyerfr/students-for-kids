c52ed137a3200b5cdfd3f99e12acf46f2088834f
Fritz Meyer <meyerfr@gmx.de>
2020-03-17 18:06:32 +0100

    fixed error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: "www.students-for-kids.de" }
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_KEY'] }
+  # config.action_mailer.delivery_method = :postmark
+  # config.action_mailer.postmark_settings = { api_key: ENV['POSTMARK_API_KEY'] }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
