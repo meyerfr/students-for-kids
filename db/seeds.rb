6c5ce14d65e575244b742f3f7cf379d7f8afc202
Fritz Meyer <meyerfr@gmx.de>
2020-03-27 14:33:07 +0100

    fixed bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@
 
 
 puts('Create Admin')
-Admin.new(email: 'hannah.meyer@students-for-kids.de', first_name: 'Hannah', last_name: 'Meyer', password: 'hannah-Meyer1')
-Admin.new(email: 'fritz.meyer@code.berlin', first_name: 'Fritz', last_name: 'Meyer', password: 'fritz-Meyer1')
+Admin.create(email: 'hannah.meyer@students-for-kids.de', first_name: 'Hannah', last_name: 'Meyer', password: 'hannah-Meyer1')
+Admin.create(email: 'fritz.meyer@code.berlin', first_name: 'Fritz', last_name: 'Meyer', password: 'fritz-Meyer1')
 
 
-puts('Create Districts')
-districts = %w(Oberkassel Niederkassel Lörick Büderich Osterath Bilk Altstadt Unterbilk Oberbilk Flehe Fliegern Düsseltal Stockum Heerdt Rath Unterrath)
+# puts('Create Districts')
+# districts = %w(Oberkassel Niederkassel Lörick Büderich Osterath Bilk Altstadt Unterbilk Oberbilk Flehe Fliegern Düsseltal Stockum Heerdt Rath Unterrath)
 
-districts.each do |d|
-  District.create(name: d)
-end
+# districts.each do |d|
+#   District.create(name: d)
+# end
